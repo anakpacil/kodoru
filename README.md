f39ab0e0905d8c4d2e7b51b9019ea25d02cc3db1
sage <laymonage@gmail.com>
2019-01-16 05:45:32 +0700

    Just use SIAK-NG
    I forgot English is also available in SIAK, duh.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Kode Organisasi UI
 
-***Web scraper*** Python yang mengeruk data dari [SIAK-NG][siak-ng] dan
-[Katalog Digital UI][dialog] untuk mendapatkan deskripsi tentang
-kode organisasi yang ada di Universitas Indonesia (UI).
+***Web scraper*** Python yang mengeruk data dari [SIAK-NG][siak-ng]
+untuk mendapatkan deskripsi tentang kode organisasi yang ada di
+Universitas Indonesia (UI).
 
 Dikembangkan menggunakan Selenium dengan bantuan [Katalon Recorder][katalon].
 
@@ -21,26 +21,22 @@ seperti berikut.
 
 ### Cara pakai
 
-Pastikan Anda sudah menginstal [`selenium`][selenium] untuk Python
-beserta [ChromeDriver][chromedriver].
-Atur *environment variable* `UI_USERNAME` dan `UI_PASSWORD` sesuai
-dengan nama pengguna dan kata sandi akun UI Anda, atau ubah
-`username` dan `password` yang ada di dalam [`orgcode.py`][orgcode].
+- Pastikan Anda sudah menginstal [`selenium`][selenium] untuk Python
+  beserta [ChromeDriver][chromedriver].
+- Atur *environment variable* `UI_USERNAME` dan `UI_PASSWORD` sesuai
+  dengan nama pengguna dan kata sandi akun UI Anda, atau ubah
+  `username` dan `password` yang ada di dalam [`orgcode.py`][orgcode].
 
-Jalankan perintah berikut di terminal atau *command prompt*.
+- Jalankan perintah berikut di terminal atau *command prompt*.
 
 ```shell
 $ python orgcode.py
 ```
 
-Tunggu hingga selesai, dan hasil akan muncul berupa tiga berkas berikut.
-
-- `org_code_siak.json`: Kode organisasi yang murni didapat dari SIAK-NG.
-- `org_code_dialog.json`: Kode organisasi SIAK-NG yang sudah dilengkapi
-  dengan data dari Katalog Digital UI.
-- `org_code.json`: Kode organisasi Katalog Digital UI yang sudah
-  dibersihkan dan dipisah antara bahasa Indonesia, bahasa Inggris,
-  dan campuran.
+- Tunggu hingga selesai dan hasil akan muncul berupa tiga berkas berikut.
+    - `org_code_id.json`: Kode organisasi dalam bahasa Indonesia.
+    - `org_code_en.json`: Kode organisasi dalam bahasa Inggris.
+    - `org_code.json`: Gabungan kedua berkas di atas.
 
 Jika Anda hanya tertarik pada data hasilnya, silakan lihat direktori
 [`dump`][dump].
@@ -50,7 +46,6 @@ Jika Anda hanya tertarik pada data hasilnya, silakan lihat direktori
 - [laymonage][laymonage]
 
 [siak-ng]: https://academic.ui.ac.id
-[dialog]: https://dialog.ui.ac.id
 [katalon]: https://chrome.google.com/webstore/detail/katalon-recorder-selenium/ljdobmomdgdljniojadhoplhkpialdid
 [selenium]: https://pypi.org/project/selenium
 [chromedriver]: http://chromedriver.chromium.org
--- orgcode.py
+++ orgcode.py
@@ -1,120 +1,88 @@
-"""Scrape SIAK-NG and Digital Catalog of UI to get description for organization codes."""
+"""Scrape SIAK-NG to get description for organization codes."""
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
-import cleanup, json, os, time, unittest
+import json, os, unittest
 
 
-class Monolithic(unittest.TestCase):
-    def _steps(self):
-        for name in dir(self):
-            if name.startswith("step"):
-                yield name, getattr(self, name)
-
-    def test_steps(self):
-        for name, step in self._steps():
-            try:
-                step()
-            except Exception as e:
-                self.fail("{} failed ({}: {})".format(step, type(e), e))
-
-
-class OCScraper(Monolithic):
-    data_siak_ng = {}
-    data_dialog = {"id": {}, "en": {}, "raw": {}}
+class OCScraper(unittest.TestCase):
+    STRINGS = {
+        "id": {
+            "fac": "Fakultas",
+            "maj": "Program Studi"
+        },
+        "en": {
+            "fac": "Faculty",
+            "maj": "Major"
+        }
+    }
 
     def setUp(self):
-        chrome_options = Options()
-        chrome_options.add_argument(
-            "--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) "
-            "AppleWebKit/602.1.50 (KHTML, like Gecko) "
-            "CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
-        ) # Desktop seems to be throttled by Dialog UI server.
-        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
         self.base_url = "https://academic.ui.ac.id/"
         self.verificationErrors = []
 
-    def step1_get_data_from_siak_ng(self):
-        data = {}
+    def authenticate(self):
         driver = self.driver
         driver.get(self.base_url + "main/Authentication/")
-        driver.find_element_by_name("u").clear()
         driver.find_element_by_name("u").send_keys(os.getenv("UI_USERNAME", "username"))
-        driver.find_element_by_name("p").clear()
         driver.find_element_by_name("p").send_keys(os.getenv("UI_PASSWORD", "password"))
         driver.find_element_by_tag_name("form").submit()
-        driver.find_element_by_link_text("Jadwal").click()
-        driver.find_element_by_link_text("Jadwal Kuliah Keseluruhan").click()
 
-        faculty_dropdown = Select(
-            driver.find_element_by_xpath(
-                "(.//*[normalize-space(text()) and normalize-space(.)='Fakultas'])[1]"
-                "/following::select[1]"
+    def scrape(self, language):
+        driver = self.driver
+        data = {}
+
+        def select_dropdown(selector):
+            return Select(
+                driver.find_element_by_xpath(
+                    "(.//*[normalize-space(text()) and normalize-space(.)='{}'])[1]"
+                    "/following::select[1]".format(selector)
+                )
             )
-        )
+
+        driver.get(self.base_url + "/main/Schedule/IndexOthers")
+        faculty_dropdown = select_dropdown(OCScraper.STRINGS[language]["fac"])
 
         faculties = [faculty.text for faculty in faculty_dropdown.options][1:]
 
         for faculty in faculties:
-            faculty_dropdown = Select(
-                driver.find_element_by_xpath(
-                    "(.//*[normalize-space(text()) and normalize-space(.)='Fakultas'])[1]"
-                    "/following::select[1]"
-                )
-            )
+            faculty_dropdown = select_dropdown(OCScraper.STRINGS[language]["fac"])
             faculty_dropdown.select_by_visible_text(faculty)
-
-            program_dropdown = Select(
-                driver.find_element_by_xpath(
-                    "(.//*[normalize-space(text()) and normalize-space(.)='Program Studi'])[1]"
-                    "/following::select[1]"
-                )
-            )
+            program_dropdown = select_dropdown(OCScraper.STRINGS[language]["maj"])
 
             for program in program_dropdown.options[1:]:
                 organization_code, program_name = program.get_attribute('value'), program.text
                 i = program_name.rfind(',')
                 study_program, educational_program = program_name[:i], program_name[i + 2:]
 
-                OCScraper.data_siak_ng[organization_code] = {
+                data[organization_code] = {
                     "faculty": faculty,
                     "study_program": study_program,
                     "educational_program": educational_program
                 }
+
+        return data
+
+    def test_get_data_from_siak_ng(self):
+        data = {lang: {} for lang in OCScraper.STRINGS}
+        driver = self.driver
+        self.authenticate()
+
+        for lang in data:
+            driver.get(self.base_url + 'main/System/Language?lang=' + lang)
+            data[lang] = self.scrape(lang)
+
         driver.get(self.base_url + 'main/Authentication/Logout')
 
-        with open("org_code_siak.json", "w") as output:
-            print(json.dumps(OCScraper.data_siak_ng, indent=2), file=output)
-
-    def step2_get_data_from_dialog(self):
-        driver = self.driver
-        self.base_url = "http://dialog.ui.ac.id/id/browse/detil/"
-        for code in OCScraper.data_siak_ng:
-            driver.get(self.base_url + code)
-            table = driver.find_element_by_id("detil")
-            trows = table.find_elements_by_tag_name("tr")
-            faculty = trows[0].find_element_by_class_name("isi_katalog").text.strip()
-            educational_program = trows[4].find_element_by_class_name("isi_katalog").text.strip()
-            study_program = trows[5].find_element_by_class_name("isi_katalog").text.strip()
-            if not faculty:
-                faculty = OCScraper.data_siak_ng[code]["faculty"].upper().replace("DAN", "&")
-            if study_program in '()':
-                study_program = OCScraper.data_siak_ng[code]["study_program"]
-            if educational_program in '()':
-                educational_program = OCScraper.data_siak_ng[code]["educational_program"]
-            result = {
-                "faculty": faculty,
-                "study_program": study_program,
-                "educational_program": educational_program,
-            }
-            OCScraper.data_dialog['raw'][code] = result
-        with open("org_code_dialog.json", "w") as output:
-            print(json.dumps(OCScraper.data_dialog, indent=2), file=output)
+        write = {"org_code.json": data}
+        write.update({"org_code_" + lang + ".json": data[lang] for lang in OCScraper.STRINGS})
+        for file_name, dic in write.items():
+            with open(file_name, "w") as output:
+                print(json.dumps(dic, indent=2), file=output)
 
     def tearDown(self):
-        cleanup.main()
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
